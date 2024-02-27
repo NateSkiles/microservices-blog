0e49217b18590ef7e02d895335ce54b16218a57a
Nathan Skiles <nateskiles94@gmail.com>
2024-02-26 16:22:21 -0800

    update start all script & post/comment lists

--- start_all.sh
+++ start_all.sh
@@ -4,13 +4,13 @@
 tmux new-session -d -s app
 
 # Start FE
-tmux new-window -t app:1 -n 'Client' 'cd client && npm start'
+tmux new-window -t app:1 -n 'client:3000' 'cd client && npm start'
 
 # Start microservice servers
-tmux new-window -t app:2 -n 'comments' 'cd comments && npm start'
-tmux new-window -t app:3 -n 'event-bus' 'cd event-bus && npm start'
-tmux new-window -t app:4 -n 'query' 'cd query && npm start'
-tmux new-window -t app:5 -n 'posts' 'cd posts && npm start'
+tmux new-window -t app:2 -n 'posts:4000' 'cd posts && npm start'
+tmux new-window -t app:3 -n 'comments:4001' 'cd comments && npm start'
+tmux new-window -t app:4 -n 'query:4002' 'cd query && npm start'
+tmux new-window -t app:5 -n 'event-bus:4003' 'cd event-bus && npm start'
 
 # Attach to tmux session
 tmux attach-session -t app
