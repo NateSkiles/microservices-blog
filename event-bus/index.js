87441b044dd8786501bd9d70c849c16c9bc6d4ba
Nathan Skiles <nateskiles94@gmail.com>
2024-02-28 13:37:03 -0800

    add event sync

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,8 +5,12 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post("/events", async (req, res) => {
   const event = req.body;
+
+  events.push(event);
 
   try {
     await axios.post("http://localhost:4000/events", event);
@@ -18,6 +22,10 @@ app.post("/events", async (req, res) => {
   }
 
   res.send({ status: "OK" });
+});
+
+app.get("/events", (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
